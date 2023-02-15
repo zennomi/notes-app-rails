ecf414b7bb5e36732acbe74b6b9a4412b899909b
zen-lenovo <zennomi.kun@gmail.com>
2023-02-15 12:37:35 +0700

    switch to postgresql

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,8 +1,8 @@
 class Page < ApplicationRecord
   belongs_to :notebook
-  has_many :paragraphs
-  has_many :image_elements
-  has_many :checklists
+  has_many :paragraphs, dependent: :delete_all
+  has_many :image_elements, dependent: :delete_all
+  has_many :checklists, dependent: :delete_all
 
   def elements
     paragraphs + image_elements + checklists
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_22_181648) do
+ActiveRecord::Schema.define(version: 2023_02_14_225648) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
     t.string "record_type", null: false
-    t.integer "record_id", null: false
+    t.bigint "record_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
@@ -25,8 +28,8 @@ ActiveRecord::Schema.define(version: 2020_06_22_181648) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -44,7 +47,7 @@ ActiveRecord::Schema.define(version: 2020_06_22_181648) do
   end
 
   create_table "checklist_items", force: :cascade do |t|
-    t.integer "checklist_id", null: false
+    t.bigint "checklist_id", null: false
     t.string "title"
     t.string "status"
     t.datetime "created_at", precision: 6, null: false
@@ -53,7 +56,7 @@ ActiveRecord::Schema.define(version: 2020_06_22_181648) do
   end
 
   create_table "checklists", force: :cascade do |t|
-    t.integer "page_id", null: false
+    t.bigint "page_id", null: false
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -61,7 +64,7 @@ ActiveRecord::Schema.define(version: 2020_06_22_181648) do
   end
 
   create_table "image_elements", force: :cascade do |t|
-    t.integer "page_id", null: false
+    t.bigint "page_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["page_id"], name: "index_image_elements_on_page_id"
@@ -69,7 +72,7 @@ ActiveRecord::Schema.define(version: 2020_06_22_181648) do
 
   create_table "notebooks", force: :cascade do |t|
     t.string "title"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_notebooks_on_user_id"
@@ -78,14 +81,14 @@ ActiveRecord::Schema.define(version: 2020_06_22_181648) do
   create_table "pages", force: :cascade do |t|
     t.string "title"
     t.integer "position"
-    t.integer "notebook_id", null: false
+    t.bigint "notebook_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["notebook_id"], name: "index_pages_on_notebook_id"
   end
 
   create_table "paragraphs", force: :cascade do |t|
-    t.integer "page_id", null: false
+    t.bigint "page_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["page_id"], name: "index_paragraphs_on_page_id"
